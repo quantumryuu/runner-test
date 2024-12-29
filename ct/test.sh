aca197b5f35a1e5ff7e13556195908a560264baa
Kristo Copani <31226503+quantumryuu@users.noreply.github.com>
2024-12-29 23:38:11 +0200

    Update test.sh

--- ct/test.sh
+++ ct/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 community-scripts ORG
-# Author: jkrgr0
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://docs.2fauth.app/
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Author: jkrgr0
 
 # App Default Values
 APP="2FAuth"
